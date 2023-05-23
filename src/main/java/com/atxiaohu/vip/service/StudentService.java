1f3883efe6de1f4e590f27604449cc17a9313349
shuaihu.zhang <1583139786@qq.com>
2023-05-23 16:12:00 +0800

    追加对应的注释，审计代码

--- src/main/java/com/atxiaohu/vip/service/StudentService.java
+++ src/main/java/com/atxiaohu/vip/service/StudentService.java
@@ -14,8 +14,9 @@ import java.util.List;
 /**
  * @Date 2023/5/19
  * @Author XiaoHu
- * @Description
+ * @Description 学生相关服务提供类
  **/
+
 @Service
 public class StudentService {
 
@@ -27,25 +28,26 @@ public class StudentService {
         this.studentMapper = studentMapper;
     }
 
+    /**
+     * 测试EasyExcel的简单读的基础功能
+     * @return 读的时间以及将读出的数据，插入数据库对应表所需的时间
+     */
     public long testRead(){
 
         List<List<Student>> lists = new ArrayList<>(10000000);
-        long start=System.currentTimeMillis();
-        //第一种方法
+        long start=System.currentTimeMillis();//时间戳开始的时间节点
+        //第一种方法---简单的读
         EasyExcel.read(FILE_PATH_READ, Student.class,
-                new PageReadListener(list ->{
-
-                    //System.out.println(list);
-                    lists.add((List<Student>)list);
+                new PageReadListener(list ->{//此时读出的数据会按照集合套集合的形式
+                    lists.add((List<Student>)list);//将从Excel读出的数据，添加到新的集合当中
                 })).sheet().doRead();
-        //System.out.println(lists.size());
 
-        Long count = 0L;
+
+        Long count = 0L;//测试从Excel中读出的数据数量是否匹配
         long middle=System.currentTimeMillis();
+        //将Excel读出的数据分批插入
         for (List<Student> list : lists) {
-//            System.out.println(list.size());
-            //System.out.println(list);
-            studentMapper.batchInsert(list);
+            studentMapper.batchInsert(list);//将集合中的数据以分批（1000条）的方式进行分批的插入数据库对应的数据表中
             count += list.size();
         }
         long end = System.currentTimeMillis();
@@ -54,16 +56,21 @@ public class StudentService {
         return count;
     }
 
+    /**
+     * 测试EasyExcel的写的能力
+     * @return 返回写出的数据是否成功，返回最终状态
+     * @throws Exception 如果在写入的过程中，有异常抛出，以便定位原因
+     */
     public String testWrite() throws Exception{
         String handlerMessage=null;
         File file=new File(FILE_PATH_WRITE);
-        if (!file.exists()){
+        if (!file.exists()){//相关路径上的文件不存在，会创建对应的相关文件
             file.createNewFile();
         }
         try {
             EasyExcel.write(FILE_PATH_WRITE,Student.class).sheet("学生信息表").doWrite(()->{
-                return studentMapper.findAll();
-            });
+                return studentMapper.findAll();//将数据库对应表的数据查出来，做成返回的数据
+            });//做写入操作
             handlerMessage="Success";
         }catch (Exception e){
             e.printStackTrace();
@@ -71,7 +78,7 @@ public class StudentService {
         }
 
 
-        return handlerMessage;
+        return handlerMessage;//返回的最终状态
     }
 
 
